9538ae1fe592e1c59a34e5152aaa3d178149d62e
Huzaifa Hussnain <huzaifa.hussnain444@tintash.com>
2020-08-11 19:42:52 +0500

    Created APIs for deleting and updating a comment. - Updated the search song functionality to match a single query string with title as well as tags of the songs.

--- song/api/urls.py
+++ song/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import SongListAPIView, SearchSongAPIView, SongDetailAPIView, PostCommentAPIView
+from .views import (
+	SongListAPIView,
+	SearchSongAPIView, 
+	SongDetailAPIView, 
+	CommentCreateAPIView, 
+	CommentUpdateAPIView,
+	CommentDeleteAPIView,
+	)
 
 urlpatterns = [
     path('list/', SongListAPIView.as_view(), name=None),
     path('details/<int:id>', SongDetailAPIView.as_view(), name=None),
     path('search/', SearchSongAPIView.as_view(), name=None),
-    path('comment/create/', PostCommentAPIView.as_view(), name=None)
+    path('comment/create/', CommentCreateAPIView.as_view(), name=None),
+    path('comment/update/<int:id>', CommentUpdateAPIView.as_view(), name=None),
+    path('comment/delete/<int:id>', CommentDeleteAPIView.as_view(), name=None),
 
 ]
--- song/api/views.py
+++ song/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
-from song.models import Song
+from song.models import Song, Comment
 from .serializers import SongSerializer, SongListSerializer, CommentSerializer
 
 
@@ -22,28 +22,49 @@ class SongDetailAPIView(generics.RetrieveAPIView):
 	lookup_field = 'id'
 
 class SearchSongAPIView(generics.ListAPIView):
-	serializer_class = SongSerializer
+	serializer_class = SongListSerializer
 
 	def get_queryset(self):
 		''' 
 		Filter the queryset based on the title 
 		and tag parameters in query string
 		'''
-		queryset = Song.objects.all()
-		title = self.request.query_params.get('title', None)
-		tag = self.request.query_params.get('tag', None)
+		q = self.request.query_params.get('q', None)
 
-		if title is not None:
-			queryset = queryset.filter(title__icontains=title)
-		if tag is not None:
-			queryset = queryset.filter(tags__icontains=tag)
+		# return empty result, if no query string is provided for search
+		if q is None:
+			queryset = None
+		else:
+			queryset = Song.objects.all()
+
+			# filtering queryset based on the query string q 
+			if q:
+				queryset = queryset.filter(title__icontains=q) | queryset.filter(tags__icontains=q)
 		return queryset
 
 
-class PostCommentAPIView(generics.CreateAPIView):
+class CommentCreateAPIView(generics.CreateAPIView):
 	serializer_class = CommentSerializer
 	
 	def perform_create(self, serializer):
 		song_id = self.request.data['song_id']
 		song = get_object_or_404(Song, id=song_id)
 		serializer.save(user=self.request.user, song=song)
+
+class CommentUpdateAPIView(generics.UpdateAPIView):
+	serializer_class = CommentSerializer
+	lookup_field = 'id'
+
+	def get_queryset(self):
+		user = self.request.user
+		return user.comment_set.all()
+
+
+class CommentDeleteAPIView(generics.DestroyAPIView):
+	serializer_class = CommentSerializer
+	lookup_field = 'id'
+
+	def get_queryset(self):
+		user = self.request.user
+		return user.comment_set.all()
+
